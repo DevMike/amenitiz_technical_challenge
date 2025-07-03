bd4d2a0d303f6b4d8650db1ba2a2a03331c5d763
Mike Zarechenskyi <mike.zarechenskyi@appsberry.com>
2025-07-03 16:01:04 +0200

    Order Checkout System

---
+++ app/order.rb
@@ -0,0 +1,20 @@
+require_relative 'product'
+
+class Order
+  def initialize
+    @basket = []
+  end
+
+  def scan(code)
+    product = PRODUCTS.detect {|product| product.code == code }
+    raise "Unknown product code: #{code}" unless product
+    @basket << product
+  end
+
+  def total
+    @basket.group_by(&:code).sum do |_, products|
+      product = products.first
+      product.pricing_rule.apply(products.count, product.price)
+    end.round(2)
+  end
+end
---
+++ spec/order_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../app/order'
+
+RSpec.describe Order do
+  let(:order) { Order.new }
+
+  describe '#total_price' do
+    subject { order.total }
+
+    shared_examples_for 'calculates total price' do |product_codes, expected_total_price|
+      it do
+        product_codes.each { |code| order.scan(code) }
+        is_expected.to eq(expected_total_price)
+      end
+    end
+
+    it_behaves_like 'calculates total price', %w[GR1 GR1], 3.11
+    it_behaves_like 'calculates total price', %w[SR1 SR1 GR1 SR1], 16.61
+    it_behaves_like 'calculates total price', %w[GR1 CF1 SR1 CF1 CF1], 30.57
+  end
+
+  describe '#scan' do
+    subject { order.scan(code) }
+
+    context 'with valid code' do
+      let(:code) { 'GR1' }
+
+      it { expect { subject }.not_to raise_error("Unknown product code: GR1") }
+    end
+
+    context 'with invalid code' do
+      let(:code) { 'XYZ' }
+
+      it { expect { subject }.to raise_error("Unknown product code: XYZ") }
+    end
+  end
+end
